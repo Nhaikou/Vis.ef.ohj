8f04ca2c8d63d6b4946ddcaa5ab0b1c034bd2203
Niko Kinnunen <nikokinnunen92@gmail.com>
2016-01-27 18:24:06 +0200

    exercise 3

--- Engine_2016/TestProjects/SimpleGraphicsTest/include/MyMaterials.h
+++ Engine_2016/TestProjects/SimpleGraphicsTest/include/MyMaterials.h
@@ -1,22 +1,25 @@
 #pragma once
 
 #include "graphics\Shader.h"
+#include "core\Object.h"
 
-struct SharedShaderValues
+struct SharedShaderValues : public core::Object
 {
 	float totalTime = 0.0f;
+	// Model view projection matrix.
+	slmath::mat4 matModelViewProj;
 };
 
 class GlobalShaderUniforms : public graphics::ShaderUniforms
 {
 public:
 	GlobalShaderUniforms(graphics::Shader* shader, const SharedShaderValues* sharedShaderValues = 0);
-	~GlobalShaderUniforms();
+	virtual~GlobalShaderUniforms();
 
 	virtual void getUniformLocations(graphics::Shader* shader);
 	virtual void bind(graphics::Shader* shader);
 
 private:
 	const SharedShaderValues* m_sharedShaderValues;
-	GLint m_id;
+	GLint m_id, m_id2;
 };
--- Engine_2016/TestProjects/SimpleGraphicsTest/src/MyMaterials.cpp
+++ Engine_2016/TestProjects/SimpleGraphicsTest/src/MyMaterials.cpp
@@ -7,7 +7,8 @@ GlobalShaderUniforms::GlobalShaderUniforms(graphics::Shader* shader, const Share
 
 void GlobalShaderUniforms::getUniformLocations(graphics::Shader* shader)
 {
-	m_id = glGetUniformLocation(shader->getProgram(), "t");
+	m_id = glGetUniformLocation(shader->getProgram(), "totalTime");
+	m_id2 = glGetUniformLocation(shader->getProgram(), "g_matModelViewProj");
 }
 
 void GlobalShaderUniforms::bind(graphics::Shader* shader)
@@ -15,7 +16,10 @@ void GlobalShaderUniforms::bind(graphics::Shader* shader)
 	shader->bind();
 	if (m_sharedShaderValues)
 	{
-		glUniform1f(m_id, m_sharedShaderValues->totalTime);
+		if (m_id2 >= 0)
+		{
+			glUniformMatrix4fv(m_id2, 1, GL_FALSE, &m_sharedShaderValues->matModelViewProj[0][0]);
+		}
 	}
 }
 
